072947be4b01567847179dac5b9093ec14b249d2
Ivan <FINDOSKDI@gmail.com>
2014-10-17 17:41:34 +0200

    Basic implementation of a LIFO structure which forgets old values. Not tested yet.

--- src/tp/pr1/logica/utilidades/CircularUnsignedIntStack.java
+++ src/tp/pr1/logica/utilidades/CircularUnsignedIntStack.java
@@ -8,27 +8,56 @@ package tp.pr1.logica.utilidades;
 
 /**
  *
- * @author marcoantonio
+ * @author ivan
  */
 public class CircularUnsignedIntStack {
-    public CircularUnsignedIntStack()
-    {
-        
-    }
-    public int count()
-    {
-        return 0;
-    }
-    public void push(int val)
-    {
-        
-    }
-    public int pop()
-    {
-        return 0;
-    }
-    public void clear()
-    {
-        
-    }
+	public CircularUnsignedIntStack() {
+		gonda = new int[10];
+	}	
+	
+	public CircularUnsignedIntStack(int size) {
+		gonda = new int[size];
+		bot = top = 0;
+	}
+
+	public int count() {
+		if (bot > top)  return top + 1 + gonda.length - bot;
+		else			return top - bot;
+	}
+
+	public void push(int val) {
+		top = inc(top);
+		if (top == bot) bot = inc(bot);
+		gonda[top] = val;
+	}
+
+	public int pop() {
+		if (bot == top) return -1;
+		else {
+			top = dec(top);
+			return gonda[inc(top)];
+		}
+	}
+
+	public void clear() {
+		int size = gonda.length;
+		if (size < 1)
+			size = 10;
+		gonda = new int[size];
+		bot = top = 0;
+	}
+	
+
+	private int inc(int n) {
+		int size = gonda.length;
+		return (n+1)%size;
+	}
+	
+	private int dec(int n) {
+		int size = gonda.length;
+		return (n-1)%size;
+	}
+	
+	private int gonda[];
+	private int bot, top;
 }
